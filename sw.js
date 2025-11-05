9fad899d156862adb3df01f00d10fbd4b856bb5b
arguetaanaryn-eng <argueta.anaryn@gmail.com>
2025-11-04 21:42:11 -0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,38 +1,24 @@
-const CACHE_NAME = 'simplebudget-v2-16-1';
+const CACHE_NAME = 'simplebudget-v2224';
 const ASSETS = [
   './',
   './index.html',
-  './app.js?v=2161',
+  './app.js',
   './style.css',
-  './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-256.png',
-  './icons/icon-512.png',
-  './icons/icon-1024.png',
-  './icons/icon-180.png'
+  './manifest.json'
 ];
-
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
 });
-
 self.addEventListener('activate', (e) => {
   e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => {
-      if (k !== CACHE_NAME) return caches.delete(k);
-    })))
+    caches.keys().then(keys => Promise.all(keys.map(k => k!==CACHE_NAME && caches.delete(k))))
   );
   self.clients.claim();
 });
-
 self.addEventListener('fetch', (e) => {
   const url = new URL(e.request.url);
   if (url.origin === location.origin) {
-    e.respondWith(
-      caches.match(e.request).then(cached => cached || fetch(e.request))
-    );
+    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
   }
 });
